2b95d8c5cbdd526383eedf573606755554cfedf7
jeanaguirre <34304106+jeanaguirre@users.noreply.github.com>
2017-12-14 14:50:31 +0100

    commit du projet le 14/12 à 14h50

---
+++ ProjPharmaEurope/src/Classes/Employe.java
@@ -0,0 +1,11 @@
+package Classes;
+
+/**
+ *
+ * @author greta
+ */
+
+//lien fichefrais avec visiteur (idVisiteur n'est pas une auto incrémentation, voir BDD (idVisiteur =a31, b246, etc.)
+public class Employe {
+    private
+}
---
+++ ProjPharmaEurope/src/lesProgrammes/TestAjoutEmploye.java
@@ -0,0 +1,20 @@
+package lesProgrammes;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import lesIHM.IHMajout;
+
+public class TestAjoutEmploye {
+    
+    
+    	public static void main(String[] args) {
+		FenetreInterface laFenetre;
+		laFenetre= new FenetreInterface();
+                laFenetre.setVisible(true);
+		// TODO Auto-generated method stub
+	}
+    
+}
